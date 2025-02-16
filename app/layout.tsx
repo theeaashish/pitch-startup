00835d3260fd58e1d3f77f75c7ef97240527d171
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-02-17 00:02:55 +0530

    sanity integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "easymde/dist/easymde.min.css";
 
 const worksans = localFont({
   src: [
