6722e9c76d51b1ec6678fd02580e0a48061ad2e8
Aashish Joshi <110759864+theeaashish@users.noreply.github.com>
2025-02-16 20:01:20 +0530

    card & hero page created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,54 @@
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 50,
+      author: { _id: 1, name: "Aashish" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://thefusioneer.com/wp-content/uploads/2023/11/5-AI-Advancements-to-Expect-in-the-Next-10-Years-scaled.jpeg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
-    HOME
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup & <br /> connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit your ideas, vote on pitches, and get noticed in virtual startup
+          competitions. Turn your vision into reality! 🚀
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search result for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
